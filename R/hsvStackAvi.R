4b595d9dbd094857bad5284599c3649febec0bf2
Kei Mochizuki <k31mczk-git@yahoo.co.jp>
2025-08-16 11:35:40 +0900

    Padding is supported in hsvStackAvi()

--- R/hsvStackAvi.R
+++ R/hsvStackAvi.R
@@ -15,6 +15,11 @@
 #' @param infiles Strings. The names of the avi files you want to merge.
 #' @param horizontal A logical. Whether to stack the videos horizontally.
 #'   If set FALSE, videos are vertically stacked.
+#' @param padding Integers of length `length(infiles) - 1`.
+#'   The sizes of paddings inserted between videos.
+#' @param col A string. The color of the padding regions.
+#'   This can be provided either as the name of the color or
+#'   in hexadecimal form (like `#000000`).
 #' @param crf An integer. The constant rate factor (crf),
 #'   i.e., a value to determine the quality of the converted video
 #'   in FFmpeg (ranging from 0 to 51).
@@ -53,6 +58,8 @@ hsvStackAvi <- function(
 
 	infiles,
 	horizontal = TRUE,
+	padding = 0,
+	col = "#000000",
 	crf = 20,
 	outfile = "",
 	suffix = "stacked",
@@ -60,6 +67,23 @@ hsvStackAvi <- function(
 	savedir = "."
 
 ) {
+
+if (length(infiles) < 2) {
+	warning("Only a single input file designated, skipping further processing")
+	return(invisible(infiles))
+}
+
+padding <- rep(padding, length(infiles) - 1)
+if (sum(padding) %% 2) {
+	i <- min(which(padding != 0))
+	padding[i] <- padding[i] + 1
+}
+print(padding)
+
+if (col %in% grDevices::colors()) {
+	col <- grDevices::rgb(t(grDevices::col2rgb("red")), maxColorValue = 255)
+}
+col <- sub("^#", "", col)
 
 checkinfiles(infiles)
 savedir <- checksavedir(savedir)
@@ -72,23 +96,24 @@ if (outfile != "") {
 }
 outfile <- file.path(savedir, outfile)
 
-filter <- ifelse(horizontal, 'hstack', 'vstack')
-if (length(infiles) > 2) {
-	filter <- paste(paste("[", 0:(length(infiles) - 1), ":v]", sep = "", collapse = ""),
-		filter, "=inputs=", length(infiles), "[v]", sep = "")
+v_org <- sprintf("[%d:v]", 0:(length(infiles) - 1))
+v_pad <- sprintf("[v%d]", 0:(length(infiles) - 1))
+flt <- ""
+if (any(padding > 0)) {
+	flt <- ifelse(horizontal, "pad=iw+%d:ih:0:0:%s", "pad=iw:ih+%d:0:0:%s")
+	flt <- sprintf(flt, padding, col)
+	flt <- paste(paste(v_org, flt, v_pad, ";", sep = "")[c(padding != 0, FALSE)], collapse = "")
 }
+flt <- paste(flt, paste(ifelse(c(padding > 0, FALSE), v_pad, v_org), collapse = ""), sep = "")
+flt <- paste(flt, ifelse(horizontal, "h", "v"), "stack=inputs=", length(infiles), "[v]", sep = "")
 
-if (length(infiles) < 2) {
-	warning("Only a single input file designated, skipping further processing")
-} else {
-	cmd <- paste(
-		paste(paste('-i "', infiles, '"', sep = ''), collapse = ' '),
-		' -codec:v libx264 -pix_fmt yuv420p -crf ', crf,
-		' -filter_complex "', filter, '"',
-		ifelse(length(infiles) > 2, ' -map "[v]"', ''),
-		' "', outfile, '"', sep = "")
-	hsvCallFFmpeg(cmd)
-}
+cmd <- paste(
+	paste(paste('-i "', infiles, '"', sep = ''), collapse = ' '),
+	' -codec:v libx264 -pix_fmt yuv420p -crf ', crf,
+	' -filter_complex "', flt, '"',
+	' -map "[v]"',
+	' "', outfile, '"', sep = "")
+hsvCallFFmpeg(cmd)
 
 if (!keepinfiles) {
 	file.remove(infiles)
